da61e427b1d937aeff768f60b1e7662ef0196178
xzdev <xiaoyuzh@gmail.com>
2016-11-22 04:20:26 -0500

    add 153 find minimum in rotated sorted array solution

---
+++ src/leetcode/q153_find_minimum_in_rotated_sorted_array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    if (!nums || nums.length <= 1) {
+        let [result] = nums;
+        return result;
+    }
+
+    return binarySearchMin(nums, 0, nums.length - 1);
+};
+
+let binarySearchMin = (nums, start, end) => {
+    if (start === end) {
+        return nums[start];
+    } else if (start + 1 === end) {
+        return Math.min(nums[start], nums[end]);
+    }
+
+    let mid = Math.floor((start + end) / 2);
+    if (nums[start] < nums[mid]) {
+        return Math.min(nums[start], binarySearchMin(nums, mid, end));
+    } else {
+        return binarySearchMin(nums, start, mid);
+    }
+}
+
+console.log(findMin([7, 0, 1, 2]));
